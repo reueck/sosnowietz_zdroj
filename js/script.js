1edb9c8429040e9c6ba8ef305541a76cc90d8ab5
reueck <65670634+reueck@users.noreply.github.com>
2022-11-17 15:06:07 +0100

    fixed listeners issue

--- js/script.js
+++ js/script.js
@@ -1,23 +1,17 @@
+//vars
 const nav = document.querySelector('.nav__bar__items')
 const navBurger = document.querySelector('.burger-btn')
 const allNavItems = document.querySelectorAll('.nav__bar__item')
 const footerYear = document.querySelector('.footer__year')
 
-
 const navHandler = () => {
 	nav.classList.toggle('nav__bar__items--active')
-
-	allNavItems.forEach(item => {
-		item.addEventListener('click', () => {
-			nav.classList.remove('nav__bar__items--active')
-		})
-	})
 	handleNavItemsAnimation()
 }
 
 const handleNavItemsAnimation = () => {
 	let delayTime = 0
-
+	
 	allNavItems.forEach(item => {
 		item.classList.toggle('nav-items-animation')
 		item.style.animationDelay = '.' + delayTime + 's'
@@ -32,3 +26,11 @@ const handleCurrentYear = () => {
 
 handleCurrentYear()
 navBurger.addEventListener('click', navHandler)
+
+
+allNavItems.forEach(item => {
+	item.addEventListener('click', () => {
+		nav.classList.remove('nav__bar__items--active')
+	})
+	item.removeEventListener
+})
